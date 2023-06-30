5cf220f9efff57e7c373eb21935bc3d88b7062aa
OscarTinajero117 <tinajero117@gmail.com>
2023-06-30 00:58:11 -0600

    add route

--- lib/encoded_string_analyzer_web/router.ex
+++ lib/encoded_string_analyzer_web/router.ex
@@ -18,6 +18,8 @@ defmodule EncodedStringAnalyzerWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    live "/encoded", EncodedLive.Index, :index
   end
 
   # Other scopes may use custom stacks.
