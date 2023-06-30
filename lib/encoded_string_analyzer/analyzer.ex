18daa4c1efbb6cbf27e060bd68423573e792e81d
OscarTinajero117 <tinajero117@gmail.com>
2023-06-30 01:20:50 -0600

    second version of analyzer (clean code)

--- lib/encoded_string_analyzer/analyzer.ex
+++ lib/encoded_string_analyzer/analyzer.ex
@@ -13,77 +13,40 @@ defmodule EncodedStringAnalyzer.Analyzer do
       Regex.replace(~r/0+/, string, "0")
       |> String.split("0")
 
-    IO.inspect(list)
-    cond do
-      length(list) == 1 ->
-        list
-        |> List.first
-        |> check_type_error(1)
-      length(list) == 2 ->
-        [first, last] = list
-
-        if first == "" && last == "" do
-          {:error, "Invalid encoded string, only zeros"}
-        else
-          {atom_first, value_first} = check_type_error(first, 2)
-          {atom_last, value_last} = check_type_error(last, 2)
-
-          cond do
-            atom_first == :num && atom_last == :num ->
-              {:error, "Invalid encoded string, Two or more IDs"}
-            atom_first == :str && atom_last == :str ->
-              {:error, "Invalid encoded string, ID is missing"}
-            atom_first == :num && atom_last == :str ->
-              if value_last == "" do
-                {:error, "Invalid encoded string, First Name and Last Name are missing"}
-              else
-                {:ok, %{first_name: value_last, last_name: "", id: value_first}}
-              end
-            atom_first == :str && atom_last == :num ->
-              if value_first == "" do
-                {:error, "Invalid encoded string, First Name and Last Name are missing"}
-              else
-                {:ok, %{first_name: value_first, last_name: "", id: value_last}}
-              end
-          end
-        end
-      length(list) == 3 ->
-        [first, middle, last] = list
-        {atom_first, value_first} = check_type_error(first, 3)
-        {atom_middle, value_middle} = check_type_error(middle, 3)
-        {atom_last, value_last} = check_type_error(last, 3)
-
-        cond do
-          atom_first == :num && atom_middle == :num && atom_last == :num ->
-            {:error, "Invalid encoded string, Two or more IDs"}
-
-          atom_first == :num && atom_middle == :num && atom_last == :str ->
-            {:error, "Invalid encoded string, Two or more IDs"}
-
-          atom_first == :num && atom_middle == :str && atom_last == :num ->
-            {:error, "Invalid encoded string, Two or more IDs"}
-
-          atom_first == :str && atom_middle == :num && atom_last == :num ->
-            {:error, "Invalid encoded string, Two or more IDs"}
-
-          atom_first == :str && atom_middle == :num && atom_last == :str ->
-            {:ok, %{first_name: value_first, last_name: value_last, id: value_middle}}
-
-          atom_first == :str && atom_middle == :str && atom_last == :num ->
-            {:ok, %{first_name: value_first, last_name: value_middle, id: value_last}}
-
-          atom_first == :num && atom_middle == :str && atom_last == :str ->
-            {:ok, %{first_name: value_middle, last_name: value_last, id: value_first}}
-
-          atom_first == :str && atom_middle == :str && atom_last == :str ->
-            {:error, "Invalid encoded string, ID is missing"}
-        end
-      true ->
-        {:error, "Invalid encoded string, More than three fields"}
+    case length(list) do
+      1 -> analyze_list(list)
+      2 -> analyze_list(list)
+      3 -> analyze_list(list)
+      _ -> {:error, "Invalid encoded string, More than three fields"}
     end
   end
 
+  defp analyze_list([first]), do: check_type_error(first, 1)
 
+  defp analyze_list([first, last]) do
+    case {check_type_error(first, 2), check_type_error(last, 2)} do
+      {{:num, _}, {:num, _}} -> {:error, "Invalid encoded string, Two or more IDs"}
+      {{:str, ""}, {:str, ""}} -> {:error, "Invalid encoded string, only zeros"}
+      {{:str, _}, {:str, _}} -> {:error, "Invalid encoded string, ID is missing"}
+      {{:num, _}, {:str, ""}} -> {:error, "Invalid encoded string, First Name and Last Name are missing"}
+      {{:num, value_first}, {:str, value_last}} -> {:ok, %{first_name: value_last, last_name: "", id: value_first}}
+      {{:str, ""}, {:num, _}} -> {:error, "Invalid encoded string, First Name and Last Name are missing"}
+      {{:str, value_first}, {:num, value_last}} -> {:ok, %{first_name: value_first, last_name: "", id: value_last}}
+    end
+  end
+
+  defp analyze_list([first, middle, last]) do
+    case {check_type_error(first, 3), check_type_error(middle, 3), check_type_error(last, 3)} do
+      {{:num, _}, {:num, _}, {:num, _}} -> {:error, "Invalid encoded string, Two or more IDs"}
+      {{:num, _}, {:num, _}, {:str, _}} -> {:error, "Invalid encoded string, Two or more IDs"}
+      {{:num, _}, {:str, _}, {:num, _}} -> {:error, "Invalid encoded string, Two or more IDs"}
+      {{:str, _}, {:num, _}, {:num, _}} -> {:error, "Invalid encoded string, Two or more IDs"}
+      {{:str, _}, {:num, _}, {:str, _}} -> {:ok, %{first_name: first, last_name: last, id: middle}}
+      {{:str, _}, {:str, _}, {:num, _}} -> {:ok, %{first_name: first, last_name: middle, id: last}}
+      {{:num, _}, {:str, _}, {:str, _}} -> {:ok, %{first_name: middle, last_name: last, id: first}}
+      {{:str, _}, {:str, _}, {:str, _}} -> {:error, "Invalid encoded string, ID is missing"}
+    end
+  end
 
   defp check_type_error(string, length_list) do
     if check_is_number?(string) do
