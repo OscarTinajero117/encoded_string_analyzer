e70ebbea23d889bd0ce4c3f811850788e6e06fff
OscarTinajero117 <tinajero117@gmail.com>
2023-06-30 19:08:23 -0600

    deploy... try again

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,19 +33,10 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "https://encoded-string-analyzer.onrender.com"
   port = String.to_integer(System.get_env("PORT") || "80")
 
   config :encoded_string_analyzer, EncodedStringAnalyzerWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
+    url: [host: "https://encoded-string-analyzer.onrender.com", port: 80, scheme: "https"],
     secret_key_base: secret_key_base,
     server: true
 
