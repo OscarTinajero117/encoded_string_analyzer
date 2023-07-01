536906ef028486f7d369bfb99593ae757184d608
OscarTinajero117 <tinajero117@gmail.com>
2023-06-30 19:16:38 -0600

    :c

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,8 +33,11 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  host = System.get_env("PHX_HOST") || "https://encoded-string-analyzer.onrender.com"
+  port = String.to_integer(System.get_env("PORT") || "80")
+
   config :encoded_string_analyzer, EncodedStringAnalyzerWeb.Endpoint,
-    url: [host: "https://encoded-string-analyzer.onrender.com", port: 80, scheme: "https"],
+    url: [host: host, port: port, scheme: "https"],
     secret_key_base: secret_key_base,
     server: true
 
